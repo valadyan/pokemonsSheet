6e08b2ad2f19d4caba0644ae185bd65d1fc18805
Your Name <you@example.com>
2025-10-17 17:12:48 +0300

    done nahuy na poka, poshel theory learning

--- src/app/card-page/card-page.ts
+++ src/app/card-page/card-page.ts
@@ -1,35 +1,39 @@
 import { Component, inject, input, signal } from '@angular/core';
 import { ApiService } from '../service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { GalleryModule, GalleryItem, ImageItem, GalleryComponent } from 'ng-gallery';
 
 import { IPokemon } from '../interfaces';
 
 @Component({
   selector: 'app-card-page',
-  imports: [GalleryComponent],
+  imports: [GalleryComponent, RouterLink],
   templateUrl: './card-page.html',
   styleUrl: './card-page.scss'
 })
 export class CardPage {
   private service = inject(ApiService);
-  private pokemonId = signal(0);
+  pokemonId = signal<number>(1);
   pokemonData: Partial<IPokemon>;
   images: GalleryItem[];
 
   private setPokemon(id: number){
-    this.service.getPokemot(id).subscribe((d: IPokemon) => {
+    this.service.getPokemon(id).subscribe((d: IPokemon) => {
       this.pokemonData = d;
       this.pokemonId.set(id);
       
-      this.images = [
-        new ImageItem({ src: d.sprites.other.home.front_default }),
-        new ImageItem({ src: d.sprites.other.home.front_female }),
-        new ImageItem({ src: d.sprites.other.home.front_shiny }),
-        new ImageItem({ src: d.sprites.other.home.front_shiny_female }),
-        new ImageItem({ src: d.sprites.other['official-artwork'].front_default }),
-        new ImageItem({ src: d.sprites.other['official-artwork'].front_shiny }),
-      ];
+      let buf: typeof this.images = []; // idk, gallery not work if push directly in this.images. and i use it
+      const filterAvaileblePathToBuf = (obj: any) => {
+        Object.keys(obj).forEach(key => {
+          const src: string = obj[key];
+          src && buf.push(new ImageItem({ src: src }));
+        })
+      }
+      filterAvaileblePathToBuf(d.sprites.other.home);
+      filterAvaileblePathToBuf(d.sprites.other['official-artwork']);
+
+      this.images = buf;
+      // and need update gallery kak-to
     });
   }
 
@@ -37,25 +41,13 @@ export class CardPage {
   private activatedRoute = inject(ActivatedRoute);
 
   constructor() {
-    this.activatedRoute.params.subscribe((params) => {
-      this.pokemonId.set(params['id']);
-    });
     this.pokemonData = {}
     this.images = [];
   } 
   ngOnInit() {
     this.setPokemon(this.pokemonId());
-  }
-  
-  goCatalog() {
-    this.router.navigate(['']);
-  }
-  prevCardClicked() {
-    this.router.navigate(['card', this.pokemonId() - 1]);
-    this.setPokemon(this.pokemonId() - 1);
-  }
-  nextNextClicked() {
-    this.router.navigate(['card', +this.pokemonId() + 1]);
-    this.setPokemon(+this.pokemonId() + 1);
+    this.activatedRoute.params.subscribe(params => 
+      this.setPokemon(params['id'])
+    );
   }
 }
